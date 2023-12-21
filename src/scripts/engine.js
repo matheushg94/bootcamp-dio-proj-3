292b53f6cc7c4dfe924422b902c6f0848e00fecb
Matheus <mat.hen.gra@gmail.com>
2023-12-21 11:34:22 -0300

    map pressable keys; dinamic toggle class active when pressing keys

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,16 +1,29 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let tune = new Audio();
+const mapedKeys = [];
 
-pianoKeys.forEach(key => {
-    key.addEventListener('mousedown', () => playTune(key.dataset.key));
-});
-
-const playTune = (key) => {
+const pressKey = (key) => {
     tune.src = `src/tunes/${key}.wav`;
-    console.log(tune);
     tune.play();
+
+    const pressedKey = document.querySelector(`[data-key="${key}"]`);
+    pressedKey.classList.add("active");
+    setTimeout(() => pressedKey.classList.remove("active"), 150);
 }
 
+pianoKeys.forEach(key => {
+    key.addEventListener('mousedown', () => pressKey(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
+});
+
 document.addEventListener('keydown', (e) => {
-    playTune(e.key);
+    /* mapedKeys.forEach(key => {
+        if (key === e.key) {
+            pressKey(e.key);
+        }
+    }) */
+
+    if (mapedKeys.includes(e.key)) {
+        pressKey(e.key);
+    }
 });
