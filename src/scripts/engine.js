779d0dd56db4d8b900c68e963dafb9a4bdc61d5d
Matheus <mat.hen.gra@gmail.com>
2023-12-21 10:34:44 -0300

    set tune play functionality

---
+++ src/scripts/engine.js
@@ -0,0 +1,12 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+let tune = new Audio();
+
+pianoKeys.forEach(key => {
+    key.addEventListener('mousedown', () => playTune(key.dataset.key));
+});
+
+const playTune = (key) => {
+    tune.src = `src/tunes/${key}.wav`;
+    console.log(tune);
+    tune.play();
+}
